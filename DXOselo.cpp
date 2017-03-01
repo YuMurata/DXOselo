34c0d80317459949c082b82aba53d4dda3f66506
YuMurata <purin7635@gmail.com>
2017-03-02 01:41:03 +0900

    regist

---
+++ DXOselo.cpp
@@ -0,0 +1,17 @@
+// DXOselo.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "DXOselo.h"
+#include<DxFunc.h>
+#include<DxLib.h>
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	DxClass obj;
+
+	WaitKey();
+}
